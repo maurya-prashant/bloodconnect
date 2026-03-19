77e7ce4b9fcf2bb5567832a5869ad816d9783089
Prashant Maurya <159791728+maurya-prashant@users.noreply.github.com>
2026-03-19 14:25:12 +0530

    updated vercel url cors

--- src/main/java/com/bloodconnect/blodconnect/config/SecurityConfig.java
+++ src/main/java/com/bloodconnect/blodconnect/config/SecurityConfig.java
@@ -60,7 +60,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://bloodconnect-app-eight.vercel.app"
+                ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
